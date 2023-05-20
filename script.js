27a43b5b63229aed3ca97bfa3baf800fd90692e1
aybikeustun <aybikeustun@gmail.com>
2023-05-20 21:32:11 +0300

    deneme1

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ const movieDetails = async (movie) => {
   renderMovie(movieRes);
 };
 
-// This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
-const fetchMovies = async () => {
-  const url = constructUrl(`movie/now_playing`);
-  const res = await fetch(url);
-  return res.json();
-};
+// // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
+// const fetchMovies = async () => {
+//   const url = constructUrl(`movie/now_playing`);
+//   const res = await fetch(url);
+//   return res.json();
+// };
 
 // Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
@@ -36,21 +36,21 @@ const fetchMovie = async (movieId) => {
   return res.json();
 };
 
-// You'll need to play with this function in order to add features and enhance the style.
-const renderMovies = (movies) => {
-  movies.map((movie) => {
-    const movieDiv = document.createElement("div");
-    movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster">
-        <h3>${movie.title}</h3>`;
-    movieDiv.addEventListener("click", () => {
-      movieDetails(movie);
-    });
-    CONTAINER.appendChild(movieDiv);
-  });
-};
+// // You'll need to play with this function in order to add features and enhance the style.
+// const renderMovies = (movies) => {
+//   movies.map((movie) => {
+//     const movieDiv = document.createElement("div");
+//     movieDiv.innerHTML = `
+//         <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
+//       movie.title
+//     } poster">
+//         <h3>${movie.title}</h3>`;
+//     movieDiv.addEventListener("click", () => {
+//       movieDetails(movie);
+//     });
+//     CONTAINER.appendChild(movieDiv);
+//   });
+// };
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovie = (movie) => {
@@ -76,4 +76,125 @@ const renderMovie = (movie) => {
     </div>`;
 };
 
+// Fetch movies and genres
+const fetchMovies = async () => {
+  const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=9c7643a821f2dd01a5781799a3e78f7c');
+  const data = await response.json();
+  const movies = data.results;
+  return movies;
+};
+
+const fetchGenres = async () => {
+  const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=9c7643a821f2dd01a5781799a3e78f7c');
+  const data = await response.json();
+  const genres = data.genres;
+  return genres;
+};
+
+// Render movies
+const renderMovies = (movies) => {
+  const movieContainer = document.querySelector('.movie-container');
+  movieContainer.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieDiv = document.createElement('div');
+    movieDiv.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster">
+      <h3>${movie.title}</h3>
+      <p>Genres: ${movie.genre_ids}</p>`;
+    movieContainer.appendChild(movieDiv);
+  });
+};
+
+// Render genre dropdown
+const renderGenreDropdown = (genres) => {
+  const genreDropdown = document.querySelector('#genre-dropdown');
+  genreDropdown.innerHTML = '';
+
+  genres.forEach((genre) => {
+    const genreLink = document.createElement('a');
+    genreLink.href = '#';
+    genreLink.textContent = genre.name;
+    genreLink.classList.add('hover:bg-yellow-300');
+    genreLink.classList.add('hover:text-black');
+    genreLink.classList.add('rounded-md');
+    genreLink.classList.add('p-1.5');
+    genreLink.addEventListener('click', () => {
+      filterMovies(genre.id);
+    });
+    genreDropdown.appendChild(genreLink);
+  });
+};
+
+// Filter movies by genre
+const filterMovies = (genreId) => {
+  const selectedGenre = genreId !== 0 ? genreId : null;
+  const moviesToShow = movies.filter((movie) => {
+    return selectedGenre ? movie.genre_ids.includes(selectedGenre) : true;
+  });
+  renderMovies(moviesToShow);
+};
+
+// Fetch and render movies and genres
+let movies, genres;
+fetchMovies()
+  .then((data) => {
+    movies = data;
+    renderMovies(movies);
+    return fetchGenres();
+  })
+  .then((data) => {
+    genres = data;
+    renderGenreDropdown(genres);
+  })
+  .catch((error) => console.log(error));
+
+
 document.addEventListener("DOMContentLoaded", autorun);
+
+
+// genre menu manipulation 
+const genreMenu = document.querySelectorAll(".genre-menu")
+const genreDropdown = document.querySelector("#genre-dropdown")
+const genreCaret = document.querySelectorAll(".genre-caret")
+
+genreMenu.forEach(menu => {
+  menu.addEventListener('click', e => {
+    genreDropdown.classList.toggle('hidden')
+    menu.classList.toggle('bg-yellow-300')
+    menu.classList.toggle('text-black')
+    genreCaret.forEach(caret => {
+      if (caret.classList.contains('fa-caret-down')) {
+        caret.classList.remove('fa-caret-down')
+        caret.classList.add('fa-caret-up')
+      }
+      else if (caret.classList.contains('fa-caret-up')) {
+        caret.classList.remove('fa-caret-up')
+        caret.classList.add('fa-caret-down')
+      }
+    })
+  })
+})
+
+// filter menu manipulation 
+const filterMenu = document.querySelectorAll(".filter-menu")
+const filterDropdown = document.querySelector("#filter-dropdown")
+const filterCaret = document.querySelectorAll(".filter-caret")
+
+filterMenu.forEach(menu => {
+  menu.addEventListener('click', e => {
+    filterDropdown.classList.toggle('hidden')
+    menu.classList.toggle('bg-yellow-300')
+    menu.classList.toggle('text-black')
+    filterCaret.forEach(caret => {
+      if (caret.classList.contains('fa-caret-down')) {
+        caret.classList.remove('fa-caret-down')
+        caret.classList.add('fa-caret-up')
+      }
+      else if (caret.classList.contains('fa-caret-up')) {
+        caret.classList.remove('fa-caret-up')
+        caret.classList.add('fa-caret-down')
+      }
+    })
+  })
+})
